a50ab13ae16879f2fdfffcb5c4b314adcbf36740
holden.wen <holden.wen@foxmail.com>
2024-04-09 21:26:01 +0800

    chore: update config

---
+++ apps/client/src/app/long-list/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "long list page",
+};
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Hello, this is a page for long list</h1>
+    </main>
+  );
+}
